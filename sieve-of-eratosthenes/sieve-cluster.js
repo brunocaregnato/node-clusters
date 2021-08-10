485ec3f95ec9ba5c2b3198b3ae5b70f49292c38a
Bruno Caregnato <bmcaregnato@ucs.br>
2021-08-09 21:26:45 -0300

    sieve with and without clusters

---
+++ sieve-of-eratosthenes/sieve-cluster.js
@@ -0,0 +1,42 @@
+const cluster = require('cluster');
+const express = require('express');
+const app = express();
+
+const cpus = !process.argv[2] ? require('os').cpus().length : process.argv[2];
+if (cluster.isMaster) {
+    for (let i = 0; i < cpus; i++) {
+        console.log(`Worker ${i} is on`);
+        cluster.fork();
+    }
+}
+else {
+    app.get('/', (req, res) => {
+        res.send(doSieveOfEratosthenes(req.query.n));
+    });
+    
+    app.listen(8080);
+}
+
+function doSieveOfEratosthenes(n) {
+    let values = [], primes = [];
+    const limit = Math.sqrt(n);
+
+    for (let i = 0; i < n; i++) values.push(true);
+
+    for (let i = 2; i <= limit; i++) {
+        if (values[i]) {
+            for (var j = i * i; j < n; j += i) values[j] = false;            
+        }
+    }
+
+    for (let i = 2; i < n; i++) {
+        if (values[i]) primes.push(i);
+    }
+
+    return primes;
+}
+
+cluster.on('exit', (worker) => {
+    console.log(`Worker with PID ${worker.process.pid} is dead! Resurrecting...`);
+    cluster.fork();
+});

bf840fd39bc13230e9779411e8458f2f853ae56e
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2021-09-30 20:54:22 -0300

    changed from mysql to mariadb

--- src/benchmarks/database.js
+++ src/benchmarks/database.js
@@ -1,11 +1,11 @@
-import mysql from 'mysql2/promise.js';
+import mariadb from 'mariadb/promise.js';
 
 export default class Database {
     async connect(){
         if(global.connection && global.connection.state !== 'disconnected')
             return global.connection;
     
-        const connect = await mysql.createConnection('mysql://root:password@localhost:3306/bancomysql');
+        const connect = await mariadb.createConnection('mariadb://root:password@localhost:3306/nodejsdb');
         global.connection = connect;
         return connect;
     }
@@ -16,7 +16,8 @@ export default class Database {
             VALUES ('Bruno Caregnato', str_to_date('12-02-1996','%m-%d-%Y'), 'M' , 'Porto Alegre', 
                 'Avenida Protasio Alves', 255)`;
         conn.query(sql).then((row) => {
-            const ordid = row[0].insertId;
+            console.log(row);
+            const ordid = row.insertId;
             sql = 'SELECT * FROM pessoa';
             conn.query(sql).then(() => {
                 sql = `UPDATE pessoa SET CIDADE = 'Caxias do Sul' WHERE ORDID = ${ordid}`;

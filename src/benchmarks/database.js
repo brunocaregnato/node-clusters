40df47f70095a40723d093140b5a828791b256cb
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2021-10-15 22:11:12 -0300

    changed createConnection ofr createPoolCluster

--- src/benchmarks/database.js
+++ src/benchmarks/database.js
@@ -5,34 +5,40 @@ export default class Database {
         if(global.connection && global.connection.state !== 'disconnected')
             return global.connection;
     
-        const connect = await mariadb.createConnection('mariadb://root:password@localhost:3306/nodejsdb');
-        global.connection = connect;
-        return connect;
+        const connection = await mariadb.createPoolCluster();
+        connection.add('master', 'mariadb://root:password@localhost:3306/nodejsdb');
+        global.connection = connection;
+        return connection;
     }
     
     async doDatabaseTest() {
-        const conn = await this.connect();
-        let sql = `INSERT INTO pessoa (NOME, NASCIMENTO, SEXO, CIDADE,ENDERECO, NUMERO) 
-            VALUES ('Bruno Caregnato', str_to_date('12-02-1996','%m-%d-%Y'), 'M' , 'Porto Alegre', 
-                'Avenida Protasio Alves', 255)`;
-        conn.query(sql).then((row) => {
-            const ordid = row.insertId;
-            sql = 'SELECT * FROM pessoa';
-            conn.query(sql).then(() => {
-                sql = `UPDATE pessoa SET CIDADE = 'Caxias do Sul' WHERE ORDID = ${ordid}`;
+        const clusterConnection = await this.connect();
+
+        await clusterConnection.getConnection().then(conn => {
+            let sql = `INSERT INTO pessoa (NOME, NASCIMENTO, SEXO, CIDADE,ENDERECO, NUMERO) 
+                VALUES ('Bruno Caregnato', str_to_date('12-02-1996','%m-%d-%Y'), 'M' , 'Porto Alegre', 
+                    'Avenida Protasio Alves', 255)`;
+            conn.query(sql).then((row) => {
+                const ordid = row.insertId;
+                sql = 'SELECT * FROM pessoa';
                 conn.query(sql).then(() => {
-                    sql = `DELETE FROM pessoa WHERE ORDID = ${ordid}`;
+                    sql = `UPDATE pessoa SET CIDADE = 'Caxias do Sul' WHERE ORDID = ${ordid}`;
                     conn.query(sql).then(() => {
-                        return;
+                        sql = `DELETE FROM pessoa WHERE ORDID = ${ordid}`;
+                        conn.query(sql).then(() => {
+                            conn.end();
+                            return;                            
+                        }).catch(error => { 
+                            throw error;
+                        });
                     }).catch(error => { 
-                       throw error;
+                        throw error;
                     });
                 }).catch(error => { 
                     throw error;
                 });
-            }).catch(error => { 
-                throw error;
             });
         });
     }
+
 }
